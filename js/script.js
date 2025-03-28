6f7823757304a74cae76904d9c802820d10aaafd
marcopriests <prtmrc98@gmail.com>
2025-03-28 15:26:20 +0100

    [UPD] snack 1 array di oggetti creato

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//SNACK 1 ----------------------------------------------------------------
+
+//creo l'array di oggetti
+const bikes = [
+    {
+        nome: "Triban RC 520 Microshift 10v",
+        peso: 10.4
+    },
+    {
+        nome: "Triban RC 120",
+        peso: 11.25
+    },
+    {
+        nome: "Van Rysel NCR CF Sram Rival AXS ETAP 12v",
+        peso: 8.6
+    },
+    {
+        nome: "Van Rysel Gravel Discovery Acolyte 8v",
+        peso: 11.5
+    },
+
+]
