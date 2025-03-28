0fadfb62dd8ef92382b442edc66a7bd0aab02206
marcopriests <prtmrc98@gmail.com>
2025-03-28 15:43:38 +0100

    [UPD] Snack 1 completato

--- js/script.js
+++ js/script.js
@@ -3,20 +3,38 @@
 //creo l'array di oggetti
 const bikes = [
     {
-        nome: "Triban RC 520 Microshift 10v",
-        peso: 10.4
+        name: "Triban RC 520 Microshift 10v",
+        weight: 10.4
     },
     {
-        nome: "Triban RC 120",
-        peso: 11.25
+        name: "Triban RC 120",
+        weight: 11.25
     },
     {
-        nome: "Van Rysel NCR CF Sram Rival AXS ETAP 12v",
-        peso: 8.6
+        name: "Van Rysel NCR CF Sram Rival AXS ETAP 12v",
+        weight: 8.6
     },
     {
-        nome: "Van Rysel Gravel Discovery Acolyte 8v",
-        peso: 11.5
+        name: "Van Rysel Gravel Discovery Acolyte 8v",
+        weight: 11.5
     },
 
 ]
+
+//creo una variabile a cui assegno il valore del peso della prima bici, mi serve per andarci a salvare man mano che ciclo le bici, quella col peso minore
+let lightBike = bikes[0]
+
+//creo il for per verificare quale bici pesa meno
+for (let i = 0; i < bikes.length; i++) {
+    
+    //effettuo un controllo per comparare il peso di ogni bici con quello salvato dentro la variabile lightBike
+    if (lightBike.weight > bikes[i].weight) {
+        lightBike = bikes[i]
+    }
+
+    
+}
+
+console.log("La bici più leggera è la " + lightBike.name + " che pesa soltanto " + lightBike.weight + "kg!!!")
+
+
